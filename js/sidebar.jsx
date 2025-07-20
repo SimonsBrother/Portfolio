3102bf3004ef21664837eb5006e355d641f0b215
SimonsBrother <calebthair@outlook.com>
2025-07-20 15:06:55 +0100

    Pushed to continue on Speccy

--- js/sidebar.jsx
+++ js/sidebar.jsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { createRoot } from 'react-dom/client';
 
-function Sidebar() {
-  return <div>
+function Sidebar(planetJsons) {
+  const [showSidebar, setShowSidebar] = useState(false)
+  const toggleSidebar = () => setShowSidebar(!showSidebar);
 
-  </div>
+  function NavBtn() {
+    return (
+      <button id="nav-btn"
+              className={showSidebar ? 'nav-btn-clicked' : 'nav-btn-default'}
+              onClick={toggleSidebar}>
+        <i className="material-symbols-outlined" id="planet-icon">planet</i>
+      </button>
+    )
+  }
+
+  // const planetEntries = planetJsons.map(planetJson => <PlanetEntry planetJson={planetJson} />)
+
+  return <>
+    <NavBtn/>
+    <ol>
+      {/*{planetEntries}*/}
+    </ol>
+  </>
 }
 
-const domNode = document.getElementById("sidebar");
-const root = createRoot(domNode);
-root.render(<Sidebar/>);
+function PlanetEntry(planetJson, id) {
+  return (
+    <li key={id}>
+      id
+    </li>
+  );
+}
+
+const sidebarNode = document.getElementById("sidebar");
+createRoot(sidebarNode).render(<Sidebar/>);
+
+// const navBtn = document.getElementById("nav-btn-container");
+// createRoot(navBtn).render(<NavBtn/>);

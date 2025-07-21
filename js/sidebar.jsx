d1526afb2c7e774aff94c5fb5699ec9f1c3f5bf1
SimonsBrother <calebthair@outlook.com>
2025-07-21 01:17:02 +0100

    Nav button is now nicely animated and displays as it should

--- js/sidebar.jsx
+++ js/sidebar.jsx
@@ -1,24 +1,45 @@
 import React, {useState} from 'react';
 import { createRoot } from 'react-dom/client';
 
-function Sidebar(planetJsons) {
-  const [showSidebar, setShowSidebar] = useState(false)
-  const toggleSidebar = () => setShowSidebar(!showSidebar);
+// Represents the different states the nav button should be in; treat this like an enum
+const NavBtnStates = {
+  Default: 0,
+  Sidebar: 1,
+  Focussed: 2,
 
-  function NavBtn() {
-    return (
-      <button id="nav-btn"
-              className={showSidebar ? 'nav-btn-clicked' : 'nav-btn-default'}
-              onClick={toggleSidebar}>
-        <i className="material-symbols-outlined" id="planet-icon">planet</i>
-      </button>
-    )
+  // The icons to show for each state, where the ID of the state represents the index of the icon name from Google icons
+  icons: [
+    "planet",
+    "arrow_back",
+    "step_out",
+  ]
+}
+
+
+function NavBtn({navState, onClick}) {
+  const clickedClass = navState === NavBtnStates.Sidebar ? 'clicked' : ''
+  return (
+    <button
+      // Only show the clicked style if the sidebar is shown, not when focussed (and obviously not when in default state)
+      className={`nav-btn ${clickedClass}`}
+      onClick={onClick}>
+      <i className={`material-symbols-outlined nav-btn-icon ${clickedClass}`}>{NavBtnStates.icons.at(navState)}</i>
+    </button>
+  )
+}
+
+function Sidebar(planetJsons) {
+  const [navState, setNavState] = useState(NavBtnStates.Default);
+  const onNavButtonClicked = () => {
+    // If the button is clicked in its default, show sidebar
+    if (navState === NavBtnStates.Default) setNavState(NavBtnStates.Sidebar);
+    // If the button is clicked and the sidebar is shown or the user is focussed on a planet, go back to default
+    else setNavState(NavBtnStates.Default);
   }
 
   // const planetEntries = planetJsons.map(planetJson => <PlanetEntry planetJson={planetJson} />)
-
   return <>
-    <NavBtn/>
+    <NavBtn navState={navState} onClick={onNavButtonClicked} />
     <ol>
       {/*{planetEntries}*/}
     </ol>
@@ -33,8 +54,5 @@ function PlanetEntry(planetJson, id) {
   );
 }
 
-const sidebarNode = document.getElementById("sidebar");
+const sidebarNode = document.getElementsByClassName("sidebar")[0];
 createRoot(sidebarNode).render(<Sidebar/>);
-
-// const navBtn = document.getElementById("nav-btn-container");
-// createRoot(navBtn).render(<NavBtn/>);

c9f20b52b2dd0171f090af3c2041566f516078d3
caleb <calebthair@outlook.com>
2025-08-25 00:39:23 +0100

    Tweaked vite build settings

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   base: '/Portfolio/',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    supported: {
+      'top-level-await': true // TODO remove use of top level awaits
+    },
   }
 })

ea603f0cd75a9b636cc9165e436e503c41fb1740
caleb <calebthair@outlook.com>
2025-08-25 00:50:11 +0100

    Tweaked project structure and build settings for CD

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/Portfolio/',
+  base: '/',
   build: {
     outDir: 'dist',
+    target: 'esnext',
     supported: {
       'top-level-await': true // TODO remove use of top level awaits
     },

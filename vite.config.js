e8e2000a11ee2d678626e76926c89794d9be6fea
caleb <calebthair@outlook.com>
2025-08-25 01:16:34 +0100

    CD tweaks

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  base: '/',
-  build: {
-    outDir: 'dist',
-    target: 'esnext',
-    supported: {
-      'top-level-await': true // TODO remove use of top level awaits
-    },
+export default defineConfig((command, mode) => {
+  console.log('Command:', command) // 'serve' or 'build'
+  console.log('Mode:', mode)       // 'development' or 'production'
+
+  const base = mode === 'production' ? '/Portfolio/' : '/'
+
+  return {
+    base,
+    build: {
+      outDir: 'dist',
+      target: 'esnext',
+      supported: {
+        'top-level-await': true // TODO remove use of top level awaits
+      },
+    }
   }
+
 })

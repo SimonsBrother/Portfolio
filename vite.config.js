ae71891826368db790d8f423f93b7e9ac28913e2
caleb <calebthair@outlook.com>
2025-08-25 15:48:35 +0100

    Testing CD

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ export default defineConfig((command, mode) => {
   console.log('Command:', command) // 'serve' or 'build'
   console.log('Mode:', mode)       // 'development' or 'production'
 
-  const base = mode === 'production' ? '/Portfolio/' : '/'
+  // const base = mode === 'production' ? '/Portfolio/' : '/'
+  const base = '/Portfolio/';
 
   return {
     base,
